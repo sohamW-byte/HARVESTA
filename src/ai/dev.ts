d86ce83689de658ae6d8ead76225d089c8c0faf3
Hardik Parab <parabhardik22@gmail.com>
2025-10-16 18:02:54 +0000

    in field section : based on data input of user give user ai recommendati

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,6 +2,6 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/crop-suggestion.ts';
+import '@/ai/flows/crop-recommendation.ts';
 import '@/ai/flows/report-generation.ts';
 import '@/ai/flows/chat-flow.ts';
