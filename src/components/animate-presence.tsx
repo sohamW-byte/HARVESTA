7eb39fcbdb198deeb6e92dcb08d25eb416898087
soham lolayekar <soham.lol0087@gmail.com>
2025-10-16 16:44:43 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/animate-presence.tsx
+++ src/components/animate-presence.tsx
@@ -2,9 +2,19 @@
 
 import { motion, AnimatePresence as FramerAnimatePresence } from 'framer-motion';
 import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export function AnimatePresence({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <FramerAnimatePresence mode="wait">
