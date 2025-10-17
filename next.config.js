0e9e373b6a48ffb4d86717cc54ad25d261110dee
shaaryan <shaaryannnn@gmail.com>
2025-10-17 04:44:49 +0000

    final version

---
+++ next.config.js
@@ -0,0 +1,39 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https' ,
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  typescript: {
+      ignoreBuildErrors: true,
+  },
+  eslint: {
+      ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
